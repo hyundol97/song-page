1a5c025bb2624618e494f39773bcaa0a9a555637
hyundol97 <gusqhdo00@naver.com>
2023-12-28 16:52:46 +0900

    feat: add home contents with Lottie files

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -4,17 +4,50 @@
  */
 
 import Lottie from 'react-lottie-player';
-import lottieJson from '../Assets/animation.json';
+import doMyBestJson from '../Assets/doMyBest.json';
+import everyExperienceJson from '../Assets/everyExperience.json';
+import richIsJson from '../Assets/richIs.json';
 
 const Home = () => {
   return (
-    <>
+    <div className="home-wrapper">
       <div className="home-main">
-        <Lottie loop animationData={lottieJson} play style={{ width: '50%', height: '50%' }} />
-        <div>ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ</div>
+        <Lottie loop animationData={doMyBestJson} play style={{ width: '50%', height: '50%' }} />
+        <div>
+          <p className="intro-main">Do My Best!</p>
+          <p className="intro-black">안녕하세요 🖐️</p>
+          <p className="intro-blue">최고보다 최선을 다하는 사람이 되고싶은</p>
+          <p className="intro-black">프론트엔드 개발자 송현석 입니다.</p>
+        </div>
       </div>
-      <div>ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ</div>
-    </>
+      <div className="home-main">
+        <Lottie loop animationData={everyExperienceJson} play style={{ width: '50%', height: '50%' }} />
+        <div>
+          <p className="intro-main">Every Experience</p>
+          <p className="intro-black">새로운 기술 및 환경을 경험하는 것, 그리고 새로운 사람을 만나는 것은</p>
+          <p className="intro-blue">또 다른 나를 발견할 수 있는 기회라고 생각합니다.</p>
+          <p className="intro-black">확신할 수 있는 길을 찾을 수 있도록</p>
+          <p className="intro-blue">찾아온 모든 기회를 쟁취하려 노력하는 편입니다.</p>
+        </div>
+      </div>
+      <div className="home-sub">
+        <Lottie loop animationData={richIsJson} play style={{ width: '50%', height: '50%' }} />
+        <div>
+          <p className="intro-black">
+            &nbsp; 세상에 부유한 사람들은 많습니다. 하지만, 여기서 의미하는 부유함은 재력에 대한 부유함을 의미합니다.
+            저는 이외에 넓고 우호적인 인간관계, 여러 사람들에게 인정받을 수 있는 능력, 그리고 여유를 가질 수 있는 심적인
+            부분 등의 방면에서 부유한 사람이 되고자 합니다. 이러한 점들을 먼저 증식시키면 재력에 대한 부유함은 저절로
+            따라오게 될 것이라고 생각합니다.
+          </p>
+          <p className="intro-black">
+            &nbsp; 세상에 부유한 사람들은 많습니다. 하지만, 여기서 의미하는 부유함은 재력에 대한 부유함을 의미합니다.
+            저는 이외에 넓고 우호적인 인간관계, 여러 사람들에게 인정받을 수 있는 능력, 그리고 여유를 가질 수 있는 심적인
+            부분 등의 방면에서 부유한 사람이 되고자 합니다. 이러한 점들을 먼저 증식시키면 재력에 대한 부유함은 저절로
+            따라오게 될 것이라고 생각합니다.
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
 
