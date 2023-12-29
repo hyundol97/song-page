7689bd986d4ad981f7f4936ef9c19c7498d4fb88
hyundol97 <gusqhdo00@naver.com>
2023-12-29 11:07:59 +0900

    feat: add footer initail UI

--- src/Components/Layout/Footer.tsx
+++ src/Components/Layout/Footer.tsx
@@ -5,7 +5,20 @@
  */
 
 const Footer = () => {
-  return <div>푸터 자리</div>;
+  return (
+    <div className="footer-wrapper">
+      <span>
+        Contact: <a href="mailto:gusqhdo00@gmail.com">gusqhdo00@gmail.com</a>
+      </span>
+      <span>
+        GitHub: <a href="https://github.com/hyundol97/reach-rich">https://github.com/hyundol97/reach-rich</a>
+      </span>
+      <span>
+        Instagram: <a href="https://www.instagram.com/hyundol_97">https://www.instagram.com/hyundol_97</a>
+      </span>
+      <span className="copyrights">Copyright 2024. hyundol97 All pictures cannot be copied without permission.</span>
+    </div>
+  );
 };
 
 export default Footer;
