36c5c4fc6b6b2d2cfff168de3b574649492e5a61
hyundol97 <gusqhdo00@naver.com>
2024-01-12 14:14:55 +0900

    feat: card component on intro page

--- src/Pages/Intro.tsx
+++ src/Pages/Intro.tsx
@@ -3,27 +3,39 @@
  * @author h.s.Song
  */
 
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 import identify from 'Assets/identify.jpg';
 
 const Intro = () => {
   return (
     <div className="intro-wrapper">
       <div className="intro-main">
-        <img src={identify} alt="identify" width="200px" />
-        <div>
-          <ol>
-            <li>이름: 송현석</li>
-            <li>성별: 남자</li>
-            <li>생년월일: 1997.08.19</li>
-            <li>거주지: 서울특별시 관악구</li>
-            <li>최종학력: 학사 (3.62/4.5)</li>
-          </ol>
-        </div>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <CardMedia component="img" image={identify} alt="green iguana" />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h5">
+                프로필
+              </Typography>
+              <Typography variant="body1">
+                <p>이름: 송현석</p>
+                <p>생년월일: 1997.08.19</p>
+                <p>성별: 남자</p>
+                <p>거주지: 서울특별시 관악구</p>
+                <p>최종학력: 학사 (3.62 / 4.5)</p>
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
       </div>
       <div className="intro-main">
         <div>
-          <p>ㅇㅁㅇㅁㅇㅁㅇㅁ</p>
-          <p>ㅁㅇㅁㅊㅋㅊㅈㅇㅂㅋ</p>
+          <p>ㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁ</p>
+          <p>ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ</p>
         </div>
       </div>
     </div>
