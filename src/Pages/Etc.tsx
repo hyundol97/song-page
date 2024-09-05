fc339bec578465d54b4b270f5ba61ccca9b5319b
hyundol97 <gusqhdo00@naver.com>
2024-09-05 22:44:36 +0900

    feat: add IntersectionObserver function init

--- src/Pages/Etc.tsx
+++ src/Pages/Etc.tsx
@@ -3,8 +3,48 @@
  * @author h.s.Song
  */
 
-const Etc = () => {
-  return <div>Etc 페이지입니다.</div>;
-};
+import { useEffect, useRef, useState } from "react";
+
+function Etc() {
+  const introRef = useRef<HTMLDivElement>(null);
+
+  const isVisible = useState<boolean>(false);
+
+  const onCheck = (entries: IntersectionObserverEntry[]) => {
+    const entry = entries;
+    console.log("check!!", entry);
+  };
+
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 1,
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(onCheck, options);
+
+    if (introRef.current) {
+      observer.observe(introRef.current);
+    }
+
+    return () => {
+      if (introRef.current) {
+        observer.unobserve(introRef.current);
+      }
+    };
+  }, []);
+
+  return (
+    <>
+      <div ref={introRef} style={{ height: "2000px" }}>
+        <div style={{ height: "500px" }}>첫 번째</div>
+        <div style={{ height: "500px" }}>두 번쨰</div>
+        <div style={{ height: "500px" }}>세 번쨰</div>
+        <div style={{ height: "500px" }}>네 번쨰</div>
+      </div>
+    </>
+  );
+}
 
 export default Etc;
